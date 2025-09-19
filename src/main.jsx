bea6ff2393ce5d5976cfea83e13ed8475bcca233
Edu Navarre <eduardo.navarre.montero@gmail.com>
2025-09-19 12:01:27 -0400

    Configured Routing in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from './App/store.js';
-import './styles/global.css';
+import { store } from './App/store';
+import App from './App';
+import Search from './Features/Search/Search';
+import PostDetail from './Features/Posts/PostDetail';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/post/:id" element={<PostDetail />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
 );
