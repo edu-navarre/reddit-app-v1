e0a1d5de79a243e59b4444dc60ccba13634a5cec
Edu Navarre <eduardo.navarre.montero@gmail.com>
2025-09-19 16:24:49 -0400

    Add PostDetail Component

---
+++ src/Features/Posts/PostDetail.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchPostDetails } from './postsSlice';
+import { useParams } from 'react-router-dom';
+
+const PostDetail = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const { post, status, error } = useSelector((state) => state.posts);
+
+  useEffect(() => {
+    dispatch(fetchPostDetails(id));
+  }, [dispatch, id]);
+
+  if (status === 'loading') return <p>Loading post...</p>;
+  if (status === 'failed') return <p>Error: {error}</p>;
+
+  return (
+    <div>
+      <h2>{post.title}</h2>
+      <p>r/{post.subreddit} • Posted by u/{post.author}</p>
+      <p>{post.selftext}</p>
+      {/* Add comments or other details here */}
+    </div>
+  );
+};
+
+export default PostDetail;
