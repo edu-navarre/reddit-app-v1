bcc4519aec54a13274d6036bcf50c961b64e46e5
Edu Navarre <eduardo.navarre.montero@gmail.com>
2025-10-06 15:43:32 -0400

    refactor: move Header and CategoryFilter to layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,50 @@
-import Search from './Features/Search/Search';
-import Categories from './Features/Categories/Categories';
-import PostsList from './Features/Posts/Posts';
-import Header from './Components/Header';
-import ErrorBoundary from './Components/ErrorBoundary';
-import './App.css';
+// import Search from './Features/Search/Search';
+// import Categories from './Features/Categories/Categories';
+// import PostsList from './Features/Posts/Posts';
+// import Header from './Components/Header';
+// import ErrorBoundary from './Components/ErrorBoundary';
+// import './App.css';
+// import { useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { fetchPosts } from './Features/Posts/postsSlice';
+// import { Outlet } from 'react-router-dom';
+
+// function App() {
+//   const dispatch = useDispatch();
+
+//   useEffect(() => {
+//     dispatch(fetchPosts());
+//   }, [dispatch]);
+
+//   return (
+//     <div style={{ padding: '0', margin: '0', boxSizing: 'border-box' }}>
+//       <Header />
+      
+//       <ErrorBoundary>
+//         <Categories />
+//       </ErrorBoundary>
+//       <div className="main-content">
+//         <ErrorBoundary>
+//           <Search />
+//         </ErrorBoundary>
+//         <ErrorBoundary>
+//           <PostsList />
+//         </ErrorBoundary>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { Outlet } from 'react-router-dom';
 import { fetchPosts } from './Features/Posts/postsSlice';
+import Header from './Components/Header';
+import Categories from './Features/Categories/Categories';
+import ErrorBoundary from './Components/ErrorBoundary';
+import './App.css';
 
 function App() {
   const dispatch = useDispatch();
@@ -18,16 +56,12 @@ function App() {
   return (
     <div style={{ padding: '0', margin: '0', boxSizing: 'border-box' }}>
       <Header />
-      
       <ErrorBoundary>
         <Categories />
       </ErrorBoundary>
       <div className="main-content">
         <ErrorBoundary>
-          <Search />
-        </ErrorBoundary>
-        <ErrorBoundary>
-          <PostsList />
+          <Outlet /> {/* This is where route-specific content will render */}
         </ErrorBoundary>
       </div>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,27 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// import { store } from './App/store';
+// import App from './App';
+// import Search from './Features/Search/Search';
+// import PostDetail from './Features/Posts/PostDetail';
+// import CategoryPage from './Features/Categories/CategoryPAge';
+// import './Styles/global.css';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <Provider store={store}>
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<App />} />
+//         <Route path="/search" element={<Search />} />
+//         <Route path="/post/:id" element={<PostDetail />} />
+//         <Route path="/category/:category" element={<CategoryPage />} />
+//       </Routes>
+//     </BrowserRouter>
+//   </Provider>
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -6,17 +30,18 @@ import { store } from './App/store';
 import App from './App';
 import Search from './Features/Search/Search';
 import PostDetail from './Features/Posts/PostDetail';
-import CategoryPage from './Features/Categories/CategoryPAge';
+import CategoryPage from './Features/Categories/CategoryPage';
 import './Styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/post/:id" element={<PostDetail />} />
-        <Route path="/category/:category" element={<CategoryPage />} />
+        <Route path="/" element={<App />}>
+          <Route index element={<Search />} />
+          <Route path="post/:id" element={<PostDetail />} />
+          <Route path="category/:category" element={<CategoryPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </Provider>
